29623df092f6dd4082488a4227bd56c95d2e8efb
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2024-04-03 12:03:00 +0530

    Exception Handling

--- src/main/java/in/purvanshh/projectservice/controllers/ProductControllers.java
+++ src/main/java/in/purvanshh/projectservice/controllers/ProductControllers.java
@@ -1,13 +1,19 @@
 package in.purvanshh.projectservice.controllers;
 
 import in.purvanshh.projectservice.dtos.FakeStoreProductDTO;
+import in.purvanshh.projectservice.dtos.ExceptionDTO;
 import in.purvanshh.projectservice.models.Product;
 import in.purvanshh.projectservice.services.FakeStoreProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.util.List;
+import java.util.Objects;
 // This enables so we can interacts with HTTPS
 @RestController
 @RequestMapping("/products")
@@ -20,8 +26,19 @@ public class ProductControllers {
     }
 
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable("id") Long id){
-            return fakeStoreProductService.getProductById(id);
+    public ResponseEntity<Object> getProductById(@PathVariable("id") Long id){
+        ResponseEntity<Object> responseEntity = null;
+        try{
+            Product product =  fakeStoreProductService.getProductById(id);
+            responseEntity = new ResponseEntity<>(product, HttpStatus.OK);
+        }catch (RuntimeException exception) {
+            ExceptionDTO exceptionDTO = new ExceptionDTO();
+            exceptionDTO.setMessage("Something Went Wrong");
+            exceptionDTO.setResolution("Do Nothing");
+            responseEntity = new ResponseEntity<>(exceptionDTO,HttpStatus.NOT_FOUND);
+        }
+
+        return responseEntity;
     }
 
 }
---
+++ src/main/java/in/purvanshh/projectservice/dtos/ExceptionDTO.java
@@ -0,0 +1,11 @@
+package in.purvanshh.projectservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionDTO {
+    private String message;
+    private String resolution;
+}
